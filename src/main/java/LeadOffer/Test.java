1c4e72750cca8f0a403cba80069ec4e257d55476
zhangxinkun <zhangxinkun@didichuxing.com>
2020-06-22 14:24:57 +0800

    add problem

--- src/main/java/LeadOffer/Test.java
+++ src/main/java/LeadOffer/Test.java
@@ -13,15 +13,22 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args){
-        Runtime.getRuntime().addShutdownHook(new Thread(){
-            @Override
-            public void run(){
-                System.out.println("jvm 退出");
-            }
-        });
+        Test test = new Test();
+        System.out.println(test.xorOperation(5,0));
+        System.out.println(test.xorOperation(1,7));
+        System.out.println(test.xorOperation(4,3));
+    }
 
-        while(true){
-            System.out.println("nihao");
+    public int xorOperation(int n, int start) {
+        int temp = 0;
+        int ans = 0;
+        if(n == 1){
+            return start;
         }
+        ans = start;
+        for(int i=1; i< n;i++){
+            ans = ans^(start+i*2);
+        }
+        return ans;
     }
 }
